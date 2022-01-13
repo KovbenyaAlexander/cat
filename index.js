55433a5c58b8a3af4bae18256bb664c66a5e1aae
KovbenyaAlexander <kovbenya.alexander1@gmail.com>
2022-01-13 13:13:51 +0300

    +

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-2022.01.13
+/*
+
+  L(light) - 1 commit
+  M(medium) - 25 commits
+  H(heavy) - 50 commits
+
+  //-----------
+
+  WEEK 1
+  SUN 2022.01.09 L
+  MON 2022.01.10 L
+  TUE 2022.01.11 L
+  WED 2022.01.12 L
+  THU 2022.01.13 H
+  FRI 2022.01.14 L
+  SAT 2022.01.15 L
+
+*/
